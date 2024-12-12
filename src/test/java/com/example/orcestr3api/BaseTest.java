d3e34004d85ce33400ed693334102a8a8bbdb1d1
A.Savin <a.savin_23@mail.ru>
2024-12-12 17:15:29 +0300

    fix

--- src/test/java/com/example/orcestr3api/BaseTest.java
+++ src/test/java/com/example/orcestr3api/BaseTest.java
@@ -1,10 +1,7 @@
 package com.example.orcestr3api;
 
 import dasniko.testcontainers.keycloak.KeycloakContainer;
-
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
